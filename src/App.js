88a6652417fc626e0a57367130769e8fe88d852f
Rohit <moza.rohit@gmail.com>
2017-08-06 19:55:18 -0400

    Landing page and project setup

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h1 id="main-heading">Cool Name</h1>
+          <div className="headButtons">
+            <button className="main-button">Sign Up</button>
+            <button className="main-button">Log In</button>
+          </div>
+        </div>
+
+        <div className="body">
+          <div id="sign-up-form">
+
+          </div>
+        </div>
+
+
+
+
+      </div>
+
+
+    );
+  }
+}
+
+export default App;
